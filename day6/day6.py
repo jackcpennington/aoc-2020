5995564054a4ff0a23fd2f58d447b93a19e17b83
Jack Pennington <jcp3g17@soton.ac.uk>
2020-12-06 20:26:14 +0000

    day 6 bpth 1 liners

--- day6/day6.py
+++ day6/day6.py
@@ -1,4 +1,8 @@
 with open('day6/input.txt') as file:
-    part_1 = sum(map(len, [set("".join([x for x in sub.split()])) for sub in file.read().split('\n\n')]))
+    part_1 = sum([len(set("".join([x for x in sub.split()]))) for sub in file.read().split('\n\n')])
 
-print ("Part 1: " + str(part_1))
+with open('day6/input.txt') as file:
+    part_2 = sum([len(set.intersection(*map(set, [x for x in sub.split()]))) for sub in file.read().split('\n\n')])
+
+print("Part 1: " + str(part_1))
+print("Part 2: " + str(part_2))
