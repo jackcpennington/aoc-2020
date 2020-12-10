0e360e82524db6e81117c01614b2212d2ed07737
Jack Pennington <jcp3g17@soton.ac.uk>
2020-12-10 15:00:50 +0000

    Completed part 2

--- day10/day10.py
+++ day10/day10.py
@@ -1,25 +1,58 @@
-from collections import Counter
+from collections import defaultdict
+
 with open("day10/input.txt") as file:
     adapters = [int(line) for line in file.read().split('\n')]
 
-print (adapters)
+def part_1(adapters):
+    adapters.sort()
+    adapters = [0] + adapters + [adapters[-1] + 3]
+    adapter_gaps = [adapters[i] - adapters[i-1] for i in range(1, len(adapters))]
+    gap_counts = dict((x,adapter_gaps.count(x)) for x in set(adapter_gaps))
+    return (gap_counts[1] * gap_counts[3])
 
-adapters.sort()
+class Graph: 
+    def __init__(self): 
+        self.graph = defaultdict(list)
+    
+    def add_edge(self,u,v): 
+        self.graph[u].append(v) 
+    
+    def memoize(func):
+        cache = dict()
 
-print (adapters)
+        def memoized_func(*args):
+            if args in cache:
+                return cache[args]
+            result = func(*args)
+            cache[args] = result
+            return result
 
-adapters = [0] + adapters + [adapters[-1] + 3]
+        return memoized_func
 
-print (adapters)
+    @memoize
+    def count_paths(self, i, j):
+        if i == j:
+            return 1
+        else:
+            result = sum(self.count_paths(c, j) for c in self.graph[i])
+            return result
 
-adapter_gaps = [adapters[i] - adapters[i-1] for i in range(1, len(adapters))]
 
-print(adapter_gaps)
+def part_2(adapters):
+    adapters.sort()
+    adapters = [0] + adapters + [adapters[-1] + 3]
 
-gap_counts = Counter(adapter_gaps)
+    possible_next_adapters = dict()
+    for i in range(len(adapters)):
+        possible_next_adapters[adapters[i]] = [a for a in adapters[i+1:i+4] if a <= adapters[i] + 3]
+    
+    g = Graph()
 
-print (gap_counts)
+    for i in possible_next_adapters:
+        for j in possible_next_adapters[i]:
+            g.add_edge(i, j)
 
-result = (gap_counts[1] * gap_counts[3])
+    return g.count_paths(adapters[0], adapters[-1])
 
-print (result)
+print ("Part 1: " + str(part_1(adapters)))
+print("Part 2: " + str(part_2(adapters)))
